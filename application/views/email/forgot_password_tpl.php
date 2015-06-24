3155bda310c8c8a06fa3817179059098a585e5f8
Le Vinh Phu <lvphu609@gmail.com>
2015-06-25 01:34:26 +0700

    template send mail

--- application/views/email/forgot_password_tpl.php
+++ application/views/email/forgot_password_tpl.php
@@ -1,1 +1,28 @@
-template test
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <p>Xin chào <?php echo $full_name;?>,</p>
+      
+    <p>Mã xác nhận của bạn là: <?php echo $code;?></p>
+
+    <p>Hoặc bạn có thể xác nhận ở liên kết bên dưới:</p>
+
+    <p><a href="<?php echo $link_reset_password; ?>"><?php echo $link_reset_password; ?></a></p>
+
+   <p>Regards,</p>
+
+  <p>X-Project</p>
+
+  <div style="background:#eee; border:1px solid #ccc; padding:5px 10px"><small>
+    Xin vui lòng không trả lời nội dung thư này vì đây là thông tin được gửi tự động bởi X-project. <br>  
+      
+    </small></div>
+
+  </body>
+</html>
+
